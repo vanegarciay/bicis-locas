4d8a66c2d86c796602ef0567813a69fb95f2ac03
VaneGarciaYara <vanegarciay@gmail.com>
2017-05-15 14:02:21 -0400

    validacion de nombre no vacio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 function validateForm(){
 	/* Escribe tú código aquí */
+    var nombre = document.getElementById("name").value;
+    var apellido = document.getElementById("lastname").value;
+    var correo = document.getElementById("input-email").value;
+    var constrasenia = document.getElementById("input-password").value;
+    var bici = document.getElementById("tipos_bici").value;
+
+    
+    if (nombre==""){
+        var nombrePadre = document.getElementsByClassName("name-container input-box")[0];
+        var mensaje = document.createElement("span");
+        var nombre_vacio = document.createTextNode('Los campos nombre deben ser completados');
+        nombrePadre.appendChild(mensaje);
+        mensaje.appendChild(nombre_vacio);
+    } else if (apellido==""){
+        
+    }
 }
